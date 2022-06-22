a44c9c77fcbb3bef0df9f7f152131336fcb436c1
JihyeonNam-0327 <n_jihyeon@naver.com>
2022-06-22 18:27:58 +0900

    save

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">후보등록</router-link> |
+    <router-link to="/voting">투표</router-link> |
+    <router-link to="/result">개표결과</router-link>
   </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ResultView from "../views/ResultView.vue";
+import DetailView from "../views/DetailView.vue";
 
 const routes = [
   {
@@ -8,13 +10,20 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/voting",
+    name: "voting",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "../views/VotingView.vue"),
+  },
+  {
+    path: "/result",
+    name: "result",
+    component: ResultView,
+  },
+  {
+    path: "/result/detail",
+    name: "detail",
+    component: DetailView,
   },
 ];
 
